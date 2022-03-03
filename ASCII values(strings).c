b42c575927987ea1b841bd6a9cc19a44f29df0aa
Ugginagithub <98165814+Ugginagithub@users.noreply.github.com>
2022-03-03 18:05:08 +0530

    Add files via upload

---
+++ ASCII values(strings).c
@@ -0,0 +1,30 @@
+/*Strings in c
+ASCII Character set
+
+
+
+
+a 97
+b 98
+c 99
+....
+z 122
+
+A 65
+B 66
+C 67
+...
+Z 90
+
+0-9----> 48-57*/
+#include<stdio.h>
+void main()
+{
+	/*char ch;
+	scanf("%c",&ch);
+	printf("ASCII VALUE OF %c is %d",ch,ch);*/
+	if('a'>'A')// 97 > 65
+	printf("yes");
+	else
+	printf("no");
+}
