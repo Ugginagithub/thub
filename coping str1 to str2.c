89065bb52f741d78c5d862ca035fcd71998f2996
Ugginagithub <98165814+Ugginagithub@users.noreply.github.com>
2022-03-03 18:06:15 +0530

    Add files via upload

---
+++ coping str1 to str2.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+/*	char str1[100]="Aditya123";
+	char str2[100];
+	int i;
+	for(i=0;str1[i]!='\0';i++)
+	{
+		str2[i]=str1[i];
+	}
+	str2[i]='\0';
+	printf("%s",str2);*/
+	
+	
+/*	char str1[100]="Aditya";
+	char str2[100]="College";
+	//strcat(dstn,src)
+	// strcat() concatenates the source
+	// string at the end of dest string
+	 printf("string 1 before concat: %s\n",str1);
+	 strcat(str1,str2);
+	 printf("string 1 after concat: %s",str1);*/
+	 
+	 
+	/* char str1[100]="Aditya";
+	 char str2[100]="College";*/
+    /*strncat(destn,src)
+    here n defines no.of characcters
+    to be appened ()*/
+    /*printf("sting 1 before concat: %s\n",str1);
+    strncat(str1,str2,2);
+    printf("string 1 after concat : %s\n",str1);*/
+    
+    
+    /* char str[100]="ADITYA";
+    // strlwr() converts string into 
+    // lower case
+    strlwr(str);
+    printf("%s",str);*/
+    
+/*    char str[100]="aditya";
+    //strupr() converts string into
+    // upper case
+    strupr(str);
+    printf("%s",str);*/
+    
+
+ /*    char str[100]="Tarun";
+     printf("Before reversal: %s\n",str);
+     //strrev () reverses the given 
+     //string in-place.
+     strrev(str);
+     printf("After reversal: %s",str);*/
+     
+     
+     
+     /*strcmp(str1,str2)
+1.gives positive values if s1 greater than s2(according to lexicographically(alphabetical order))
+2.gives negative values if s1 less than s2(according to lexicographically (alphabetical order))
+2.gives 0 if s1 equals s2(according to lexicographically (alphabetical order))*/
+	/*char str1[100]="god";
+	char str2[100]="dog";
+	int res = strcmp(str1,str2);
+	printf("%d",res);*/
+	
+/*strncpy (destn,src,n) copies the amount (n) of characters from source string to destination string*/
+ /*char str1[100]="Adtiya";
+ char str2[100];
+  strncpy(str2,str1,2);
+ printf("%d",str2);*/
+
+
+}
